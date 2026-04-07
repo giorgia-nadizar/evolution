3b95d167668e9348f1a53730b7eeaad6c9b5c1fb
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2026-04-07 11:25:28 +0200

    competitive coevolution notebook exercises

---
+++ coevolution/solutions.py
@@ -0,0 +1,38 @@
+# competitive coevolution
+
+def evaluate_tournament(predators, prey, k=5):
+    predator_fitness = np.zeros(len(predators))
+    prey_fitness = np.zeros(len(prey))
+
+    for i, p in enumerate(predators):
+        opponents = random.sample(list(prey), k)
+        for q in opponents:
+            distance = abs(p - q)
+            predator_fitness[i] += -distance
+
+    for j, q in enumerate(prey):
+        opponents = random.sample(list(predators), k)
+        for p in opponents:
+            distance = abs(p - q)
+            prey_fitness[j] += distance
+
+    return predator_fitness, prey_fitness
+
+
+def evaluate_with_hof(predators, prey, hof_pred, hof_prey):
+    predator_fitness = np.zeros(len(predators))
+    prey_fitness = np.zeros(len(prey))
+
+    for i, p in enumerate(predators):
+        opponents = list(prey) + hof_prey
+        for q in opponents:
+            distance = abs(p - q)
+            predator_fitness[i] += -distance
+
+    for j, q in enumerate(prey):
+        opponents = list(predators) + hof_pred
+        for p in opponents:
+            distance = abs(p - q)
+            prey_fitness[j] += distance
+
+    return predator_fitness, prey_fitness
